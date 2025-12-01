ddb06375647311b86e5e57382f9fde66793d69e6
a106089-dot <a106089@hotmail.com>
2025-12-01 17:04:16 +0800

    Add files via upload
    divide airline color code

---
+++ airlineColors.js
@@ -0,0 +1,21 @@
+// airlineColors.js
+
+const airlineColors = {
+  "CI": "#5A3E85",
+  "BR": "#007A3D",
+  "AE": "#005BAC",
+  "IT": "#FF8800",
+  "JX": "#C2A56C",
+  "NH": "#0B69C6",
+  "JL": "#DA0027",
+  "MM": "#C3379F",
+  "GK": "#FF6E00",
+  "BC": "#1B3F8B",
+  "7G": "#C7D301",
+  "HD": "#78C4E0",
+  "KE": "#00A9E0",
+  "OZ": "#B31F37",
+  "CX": "#046A7A",
+  "SQ": "#002663",
+  "TR": "#FFCC00"
+};
